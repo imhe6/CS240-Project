458ef3bf1234db14c084190bf6441bd1d4f94c09
Qihan Xu <95729258+imhe6@users.noreply.github.com>
2026-05-21 16:42:39 +0800

    Untrack build files in submodules, revise .gitignore and python script, add run flag to bash scripts

--- scripts/run_mip360.sh
+++ scripts/run_mip360.sh
@@ -1,29 +1,46 @@
 GPU=0
 
+# parse arguments for output directory
+if [ "$1" == "-o" ]; then
+    OUTPUT_DIR="$2"
+    shift 2
+else
+    echo "Usage: $0 -o <output_dir>"
+    exit 1
+fi
+
+CWD=$(pwd)
+ABS_OUTPUT_DIR="${CWD}/${OUTPUT_DIR}"
+
+LOC=$(dirname "$0")
+cd $LOC
+
 
 # Preprocess (downscale datasets)
-CUDA_VISIBLE_DEVICES=$GPU python scripts/downscale_dataset_mip360.py \
---config configs/mip360.yml
+CUDA_VISIBLE_DEVICES=$GPU python downscale_dataset_mip360.py \
+--config ../configs/mip360.yml
 
 
 # Run Gaussian Splatting
-scenes=(bicycle bonsai counter garden kitchen room stump flowers treehill)
+# flowers and treehill are not publicly available
+# scenes=(bicycle bonsai counter garden kitchen room stump flowers treehill)
+scenes=(bicycle bonsai counter garden kitchen room stump)
 for scene in "${scenes[@]}";
 do
     # Training
-    CUDA_VISIBLE_DEVICES=$GPU python train.py \
-        -m "output/mip360/${scene}" \
+    CUDA_VISIBLE_DEVICES=$GPU python ../train.py \
+        -m "${ABS_OUTPUT_DIR}/mip360/${scene}" \
         -i "images_vsr" --eval -r 1 \
-        --config configs/mip360.yml
+        --config ../configs/mip360.yml
 
     # Rendering 
-    CUDA_VISIBLE_DEVICES=$GPU python render_samename.py \
-        -m "output/mip360/${scene}" \
+    CUDA_VISIBLE_DEVICES=$GPU python ../render_samename.py \
+        -m "${ABS_OUTPUT_DIR}/mip360/${scene}" \
         -i "images_gt" --eval -r 1 --skip_train \
-        --config configs/mip360.yml
+        --config ../configs/mip360.yml
 
     # Metric
-    CUDA_VISIBLE_DEVICES=$GPU python metrics.py \
-        -m "output/mip360/${scene}"
+    CUDA_VISIBLE_DEVICES=$GPU python ../metrics.py \
+        -m "${ABS_OUTPUT_DIR}/mip360/${scene}"
 done
 
